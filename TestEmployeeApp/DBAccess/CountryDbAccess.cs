403e4e8fb4b0b5d3a6b6d8390e4bee7f8ed8b4ef
subhabrata6 <subhabrata.saha@codeclouds.in>
2024-05-16 20:20:53 +0530

    today's work

--- TestEmployeeApp/DBAccess/CountryDbAccess.cs
+++ TestEmployeeApp/DBAccess/CountryDbAccess.cs
@@ -136,5 +136,122 @@ namespace TestEmployeeApp.DBAccess
 
             return response;
         }
+
+        public ResponseMessage DeleteCountryById(int id)
+        {
+            ResponseMessage response = new ResponseMessage();
+
+            string connStr = Helper.GetConnectionString(_configuration);
+
+            SqlConnection conn = null;
+            SqlCommand cmd = null;
+            SqlTransaction transaction = null;
+
+            try
+            {
+                conn = new SqlConnection(connStr);
+                conn.Open();
+                transaction = conn.BeginTransaction();
+                cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "DELETE_COUNTRY_BY_ID";
+                cmd.Transaction = transaction;
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    transaction.Commit();
+                    response.Status = HttpStatusCode.OK;
+                    response.Message = "Deleted successfully";
+                }
+                else
+                {
+                    transaction.Rollback();
+                    response.Status = HttpStatusCode.BadRequest;
+                    response.Message = "Failed to delete this country";
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Dispose();
+            }
+
+            return response;
+        }
+
+        public ResponseMessage GetCountryById(int id)
+        {
+            ResponseMessage response = new ResponseMessage();
+
+            string connStr = Helper.GetConnectionString(_configuration);
+
+            SqlConnection conn = null;
+            SqlDataAdapter da = null;
+            SqlCommand cmd = null;
+            DataTable countryDt = new DataTable();
+            Country country = new Country();
+
+            try
+            {
+                conn = new SqlConnection(connStr);
+                conn.Open();
+                cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "GET_COUNTRY_BY_ID";
+                cmd.Parameters.AddWithValue("@ID", id);
+                da = new SqlDataAdapter(cmd);
+                da.Fill(countryDt);
+
+                if (countryDt.Rows.Count > 0)
+                {
+                    country.countryName = Helper.GetDBStringValue(countryDt.Rows[0]["COUNTRY_NAME"]);
+                    country.createdOn = Helper.GetDBStringValue(countryDt.Rows[0]["CREATED_ON"]);
+                    country.id = Helper.GetDBIntValue(countryDt.Rows[0]["ID"]);
+
+
+
+                    response.Status = HttpStatusCode.OK;
+                    response.Response = country;
+                    response.Message = "OK";
+                }
+                else
+                {
+                    response.Status = HttpStatusCode.NoContent;
+                    response.Response = country;
+                    response.Message = "No data found";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Dispose();
+                da.Dispose();
+            }
+
+            return response;
+        }
     }
 }
--- TestEmployeeApp/Program.cs
+++ TestEmployeeApp/Program.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using TestEmployeeApp.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            options.Cookie.SameSite = SameSiteMode.Strict;
+            options.LoginPath = "/Account/Login";
+            options.AccessDeniedPath = "/Account/Login";
+
+            // Configure other options as needed
+        });
 
 var app = builder.Build();
 
@@ -15,18 +32,22 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
-
+app.UseAuthentication();
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+        pattern: "{controller=Account}/{action=Login}/{id?}");
 });
 
 app.MapControllers();
-
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/Account/Login");
+    return Task.CompletedTask;
+});
 app.MapRazorPages();
 
 app.Run();
